b3e7c12a6d1cfa8ffdd1c053cc57f3654a1c2efc
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-08-23 14:40:36 +0700

    Google Analytics

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layouts/Header";
 import Footer from "@/components/layouts/Footer";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <GoogleAnalytics trackPageViews gaMeasurementId="G-0BL83YPNVY" />
       </body>
     </html>
   );
