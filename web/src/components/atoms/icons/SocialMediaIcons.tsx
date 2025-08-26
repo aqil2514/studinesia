a4266ed9f18c7158c2e1aa18864c2f72241555a0
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-08-26 08:41:22 +0700

    twitter sosmed

--- web/src/components/atoms/icons/SocialMediaIcons.tsx
+++ web/src/components/atoms/icons/SocialMediaIcons.tsx
@@ -1,15 +1,15 @@
 import { BasicIcons } from "@/@types/icons";
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
 
 export const SocialMediaIcons: BasicIcons[] = [
   {
     Icon: FaFacebook,
     link: "https://facebook.com/studinesia",
   },
-  // {
-  //   Icon: FaXTwitter,
-  //   link: "#",
-  // },
+  {
+    Icon: FaTwitter,
+    link: "https://x.com/studinesiaid?t=BNgmQJ-asG3lUMroMXSJXA&s=09",
+  },
   // {
   //   Icon: FaTiktok,
   //   link: "#",
